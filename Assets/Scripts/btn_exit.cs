a06ad69ad2e49d9be009a0fa8921065156003c47
Jonas Wiesli <50823899+Joeni-McBoeni@users.noreply.github.com>
2020-02-28 13:20:21 +0100

    finishing touches

---
+++ Assets/Scripts/btn_exit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btn_exit : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Application.Quit();
+    }
+}
